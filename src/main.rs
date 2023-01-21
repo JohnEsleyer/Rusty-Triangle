eed5704b3ca068969b1185777c1aa1defdffe49d
john <ralphesleyer@gmail.com>
2023-01-21 16:13:22 +0800

    added renderer loop

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 extern crate sdl2;
 
 fn main(){
-    let _sdl = sdl2::init().unwrap();
+    let sdl = sdl2::init().unwrap();
+    let video_subsystem = sdl.video().unwrap();
+    let window = video_subsystem
+        .window("Game", 900, 700)
+        .resizable()
+        .build()
+        .unwrap();
+    
+
+    let mut event_pump = sdl.event_pump().unwrap();
+    loop {
+        for _event in event_pump.poll_iter(){
+            // handle user input here
+        }
+
+        // render window contents here
+    }
 }
