5d8a5b72f25ab96a55c4cbcca24e688807614e35
john <ralphesleyer@gmail.com>
2023-01-21 16:18:03 +0800

    close window

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,11 @@ fn main(){
     
 
     let mut event_pump = sdl.event_pump().unwrap();
-    loop {
-        for _event in event_pump.poll_iter(){
-            // handle user input here
+    'main: loop {
+        for event in event_pump.poll_iter(){
+            match event {
+                sdl2::event::Event::Quit{..} => break 'main, _ => {},
+            }
         }
 
         // render window contents here
