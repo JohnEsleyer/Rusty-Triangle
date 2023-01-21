c3cb48c3cdd5efe813b9eab2be8297d16eb995ba
john <ralphesleyer@gmail.com>
2023-01-21 17:01:27 +0800

    compiled shaders

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 extern crate gl;
 extern crate sdl2;
 
+pub mod render_gl;
+
 fn main() {
     let sdl = sdl2::init().unwrap();
     let video_subsystem = sdl.video().unwrap();
+
+    let gl_attr = video_subsystem.gl_attr();
+
+    gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+    gl_attr.set_context_version(4, 1);
+
     let window = video_subsystem
         .window("Game", 900, 700)
         .opengl()
@@ -16,8 +24,22 @@ fn main() {
         gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
 
     unsafe {
+        gl::Viewport(0, 0, 900, 700);
         gl::ClearColor(0.3, 0.3, 0.5, 1.0);
     }
+
+    use std::ffi::CString;
+    let vert_shader =
+        render_gl::Shader::from_vert_source(&CString::new(include_str!("triangle.vert")).unwrap())
+            .unwrap();
+
+    let frag_shader =
+        render_gl::Shader::from_frag_source(&CString::new(include_str!("triangle.frag")).unwrap())
+            .unwrap();
+
+    let shader_program = render_gl::Program::from_shaders(&[vert_shader, frag_shader]).unwrap();
+
+    shader_program.set_used();
 
     let mut event_pump = sdl.event_pump().unwrap();
     'main: loop {
